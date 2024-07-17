7e81b7c447829066fd0ec27c8e9c525dd274a625
moussaillon <moussaillon@Mac-mini-17.local>
2024-07-17 10:26:57 +0200

    Installation Voter en process

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(Security $security): Response
     {
+        if ($security->getUser()) {
+
+            $user = $security->getUser();
+        } else {
+            $user = "no_user";
+        }
+      
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'user' => $user
         ]);
-    }
+     }
 }

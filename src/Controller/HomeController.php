2ec39921d9959525a8f66895b5d0cb77c5a90e13
moussaillon <moussaillon@Mac-mini-17.local>
2024-07-19 16:53:57 +0200

    earthquake

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,9 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Entity\Earthquake;
 use App\Repository\EarthquakeRepository;
+
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
+
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -13,16 +18,23 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(Security $security, EarthquakeRepository $earthquakeRepository): Response
+    public function index(Security $security, EarthquakeRepository $earthquakeRepository , PaginatorInterface $paginator, Request $request): Response
     {
-      
-        $datas=  $this->json([
-            ['name' => 'Site 1', 'latitude' => 46.56, 'longitude', 7.3],
-            ['name' => 'Site 2', 'latitude' => 44.38, 'longitude', 6.89],
-        ]);
+
+        $query = $earthquakeRepository->findAll();
+    
+        $pagination = $paginator->paginate(
+            $query, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            10 /*limit per page*/
+        );
+    
+ 
+        
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'datas' => $datas
+            'pagination' => $pagination
+
         ]);
      }
 }
--- src/Entity/Earthquake.php
+++ src/Entity/Earthquake.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\EarthquakeRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: EarthquakeRepository::class)]
 class Earthquake
@@ -12,18 +13,21 @@ class Earthquake
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['earthquakes.index'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
     private ?string $time = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['earthquakes.index'])]
     private ?string $latitude = null;
 
     #[ORM\Column(name: 'idToken', length: 255)]
     private ?string $idToken = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['earthquakes.index'])]
     private ?string $longitude = null;
 
     #[ORM\Column(length: 255)]
@@ -69,6 +73,7 @@ class Earthquake
     private ?string $magNst = null;
 
     #[ORM\Column(length: 2, name: 'locationSource', nullable: true)]
+    #[Groups(['earthquakes.index'])]
     private ?string $locationSource = null;
 
     #[ORM\Column(length: 2, name: 'magSource', nullable: true)]

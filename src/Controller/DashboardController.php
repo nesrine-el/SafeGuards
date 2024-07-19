2860d5b5e7597f5b6699a98f6885a677872a8531
Bleza PLEGNON <bleza.plg@gmail.com>
2024-07-19 16:09:25 +0200

    Mise en place du dashboard utilisateur

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractController
+{
+    #[Route('/user/{id}/dashboard', name: 'user_dashboard')]
+    public function dashboard(int $id, ArticleRepository $articleRepository, EntityManagerInterface $entityManager): Response
+    {
+        $user = $entityManager->getRepository(User::class)->find($id);
+
+        
+        $articles = [
+            ['title' => 'Titre de mon article', 'date' => 'Date de création', 'likes' => 15, 'comments' => 15],
+            ['title' => 'Titre de mon article', 'date' => 'Date de création', 'likes' => 15, 'comments' => 15],
+            ['title' => 'Titre de mon article', 'date' => 'Date de création', 'likes' => 15, 'comments' => 15],
+        ];
+
+        $favoriteArticles = ['Article 1', 'Article 2', 'Article 3'];
+        $favoriteAuthors = ['Auteur 1', 'Auteur 2', 'Auteur 3'];
+
+        return $this->render('dashboard/index.html.twig', [
+            'user' => $user,
+            'articles' => $articles,
+            'favoriteArticles' => $favoriteArticles,
+            'favoriteAuthors' => $favoriteAuthors,
+        ]);
+    }
+}
